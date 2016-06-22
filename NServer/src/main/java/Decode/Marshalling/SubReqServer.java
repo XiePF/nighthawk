66e72632733976ec7b72d49095f4d374e19f4156
xiepf <xiepf.sky@gmail.com>
2016-06-22 19:44:26 +0800

    start JBoss Marshalling

---
+++ NServer/src/main/java/Decode/Marshalling/SubReqServer.java
@@ -0,0 +1,61 @@
+package Decode.Marshalling;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * JBoss Marshalling
+ * Created by Administrator on 2016/06/22.
+ */
+public class SubReqServer {
+
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChildChannelHandler());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(
+            MarshallingCodeCFactory.buildMarshallingDecoder());
+            socketChannel.pipeline().addLast(
+                    MarshallingCodeCFactory.buildMarshallingEncoder());
+            socketChannel.pipeline()
+                    .addLast(new SubReqServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        new SubReqServer().bind(port);
+    }
+}
