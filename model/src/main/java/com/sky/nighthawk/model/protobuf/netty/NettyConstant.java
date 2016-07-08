6a56a4a7ae29bd901a5d6ebefe940e8c0d96dcf5
xiepf <xiepf.sky@gmail.com>
2016-07-08 11:40:47 +0800

    私有协议栈开发：定义消息类型

---
+++ model/src/main/java/com/sky/nighthawk/model/protobuf/netty/NettyConstant.java
@@ -0,0 +1,11 @@
+package com.sky.nighthawk.model.protobuf.netty;
+
+/**
+ * Created by xpf on 2016/07/08.
+ */
+public final class NettyConstant {
+    public static final String REMOTEIP = "127.0.0.1";
+    public static final int PORT = 8080;
+    public static final int LOCAL_PORT = 12088;
+    public static final String LOCALIP = "127.0.0.1";
+}
