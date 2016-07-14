cd02935237e6026c82c1e54d31424707325f6469
xiepf <xiepf.sky@gmail.com>
2016-07-14 14:26:24 +0800

    私有协议栈开发 客户端开发

--- NClient/src/main/java/com/sky/nighthawk/model/protobuf/netty/NettyClient.java
+++ NClient/src/main/java/com/sky/nighthawk/model/protobuf/netty/NettyClient.java
@@ -10,7 +10,6 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.timeout.ReadTimeoutHandler;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
@@ -21,68 +20,55 @@ import java.util.concurrent.TimeUnit;
  * Created by xpf on 2016/07/11.
  */
 public class NettyClient{
-private ScheduledExecutorService executor = Executors
-        .newScheduledThreadPool(1);
-EventLoopGroup group = new NioEventLoopGroup();
+    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    EventLoopGroup group = new NioEventLoopGroup();
 
-        public void connect(int port, String host) throws Exception {
+    public void connect(int port, String host,final int localPort) throws Exception {
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //ch.pipeline().addLast(MarshallingFactory.buildMarshallingDecoder());
 
-            // 配置客户端NIO线程组
+                            ch.pipeline().addLast(new NettyMessageDecoder(1024*1024, 4, 4,-8,0));
+                            ch.pipeline().addLast("MessageEncoder", new NettyMessageEncoder());
 
-            try {
-                Bootstrap b = new Bootstrap();
-                b.group(group).channel(NioSocketChannel.class)
-                        .option(ChannelOption.TCP_NODELAY, true)
-                        .handler(new ChannelInitializer<SocketChannel>() {
-                            @Override
-                            public void initChannel(SocketChannel ch)
-                                    throws Exception {
-                                ch.pipeline().addLast(
-                                        new NettyMessageDecoder(1024 * 1024, 4, 4));
-                                ch.pipeline().addLast("MessageEncoder",
-                                        new NettyMessageEncoder());
-                                ch.pipeline().addLast("readTimeoutHandler",
-                                        new ReadTimeoutHandler(50));
-                                ch.pipeline().addLast("LoginAuthHandler",
-                                        new LoginAuthReqHandler());
-                                ch.pipeline().addLast("HeartBeatHandler",
-                                        new HeartBeatReqHandler());
-                            }
-                        });
-                // 发起异步连接操作
-                ChannelFuture future = b.connect(
-                        new InetSocketAddress(host, port),
-                        new InetSocketAddress(NettyConstant.LOCALIP,
-                                NettyConstant.LOCAL_PORT)).sync();
-//                ChannelFuture future = b.connect(NettyConstant.LOCALIP, NettyConstant.LOCAL_PORT).sync();
-                future.channel().closeFuture().sync();
-            } finally {
-                // 所有资源释放完成之后，清空资源，再次发起重连操作
-                executor.execute(new Runnable() {
-                    @Override
-                    public void run() {
+                            //ch.pipeline().addLast(MarshallingFactory.buildMarshallingEncoder());
+                            ch.pipeline().addLast("LoginAuthHandler", new LoginAuthReqHandler());
+                            ch.pipeline().addLast("HeartBeatHandler", new HeartBeatReqHandler());
+                        }
+                    });
+            //发起异步操作
+            ChannelFuture future = b.connect(new InetSocketAddress(host, port),
+                    new InetSocketAddress(NettyConstant.LOCALIP, localPort)).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            //断线重连
+            executorService.execute(new Runnable() {
+                public void run() {
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
                         try {
-                            TimeUnit.SECONDS.sleep(1);
-                            try {
-                                connect(NettyConstant.PORT, NettyConstant.REMOTEIP);// 发起重连操作
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        } catch (InterruptedException e) {
+                            connect(NettyConstant.PORT, NettyConstant.REMOTEIP,localPort);
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                });
-            }
+                }
+            });
         }
+    }
 
-        /**
-         * @param args
-         * @throws Exception
-         */
-        public static void main(String[] args) throws Exception {
-            new NettyClient().connect(NettyConstant.PORT, NettyConstant.REMOTEIP);
+    public static void main(String[] args) {
+        try {
+            new NettyClient().connect(NettyConstant.PORT, NettyConstant.REMOTEIP,7395);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
+    }
 }
-
