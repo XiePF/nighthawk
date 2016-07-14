c51448485d22fd080f0ed5c4e751b1f38908c968
xiepf <xiepf.sky@gmail.com>
2016-07-14 14:27:17 +0800

    私有协议栈开发 服务端开发

--- NServer/src/main/java/com/sky/nighthawk/model/protobuf/netty/NettyServer.java
+++ NServer/src/main/java/com/sky/nighthawk/model/protobuf/netty/NettyServer.java
@@ -12,46 +12,46 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.timeout.ReadTimeoutHandler;
 
-import java.io.IOException;
 
 /**
  * Created by xpf on 2016/07/12.
  */
 public class NettyServer {
     public void bind() throws Exception {
-        // 配置服务端的NIO线程组
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         ServerBootstrap b = new ServerBootstrap();
-        b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_BACKLOG, 100)
-                .handler(new LoggingHandler(LogLevel.INFO))
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel ch)
-                            throws IOException {
-                        ch.pipeline().addLast(
-                                new NettyMessageDecoder(1024 * 1024, 4, 4));
-                        ch.pipeline().addLast(new NettyMessageEncoder());
-                        ch.pipeline().addLast("readTimeoutHandler",
-                                new ReadTimeoutHandler(50));
-                        ch.pipeline().addLast(new LoginAuthRespHandler());
-                        ch.pipeline().addLast("HeartBeatHandler",
-                                new HeartBeatRespHandler());
-                    }
-                });
 
-        // 绑定端口，同步等待成功
-        ChannelFuture f =  b.bind(NettyConstant.REMOTEIP, NettyConstant.PORT).sync();
-        f.channel().closeFuture().sync();
-        System.out.println("Netty server start ok : "
-                + (NettyConstant.REMOTEIP + " : " + NettyConstant.PORT));
+        try {
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            // 第四个参数是调整因为前2个有关长度的值导致（LengthFieldBasedFrameDecoder里面会把
+                            // frameLength与（message的长度加上length有关的偏移量）进行比较，如果不修整会出错
+                            ch.pipeline().addLast(new NettyMessageDecoder(1024 * 1024, 4, 4, -8, 0));
+                            ch.pipeline().addLast(new NettyMessageEncoder());
+                            ch.pipeline().addLast(new LoginAuthRespHandler());
+                            ch.pipeline().addLast(new HeartBeatRespHandler());
+                        }
+                    });
+            ChannelFuture f = b.bind(NettyConstant.REMOTEIP, NettyConstant.PORT).sync();
+            System.out.println("Netty server start ok : " + (NettyConstant.REMOTEIP + " : " + NettyConstant.PORT));
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 
-    public static void main(String[] args) throws Exception {
-        new NettyServer().bind();
+    public static void main(String[] args) {
+        try {
+            new NettyServer().bind();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
-
